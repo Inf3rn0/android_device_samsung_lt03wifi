186a82a8bf38cc432fdfd4cf0e0d567a446ce731
Inf3rn0 <gary@infnet.nl>
2016-08-21 12:37:05 +0200

    Bye Gello!
    Change-Id: Ic7eaf5fc0fe1df4f15360b877541588223898e20

--- device.mk
+++ device.mk
@@ -87,10 +87,6 @@ PRODUCT_PACKAGES += \
     e2fsck \
     setup_fs
 
-# Gello
-PRODUCT_PACKAGES += \
-    Gello
-
 # GPS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
